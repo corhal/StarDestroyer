7b06acdfbfeaba349b9c6222e1779824ed814836
Andrey Panfilov <a.panfilov@nexters.com>
2016-08-25 13:35:21 +0300

    First batch of files

---
+++ Assets/Scripts/ShipModule.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class ShipModule : MonoBehaviour {	
+	public int StartingHp;
+	int maxHp;
+	int hp;
+	bool operational;
+
+	// Use this for initialization
+	void Start () {
+		maxHp = StartingHp;
+		hp = maxHp;
+		operational = true;
+	}
+	
+	public void TakeDamage(int damage) {
+		hp -= damage;
+		if () {
+			
+		}
+	}
+}
